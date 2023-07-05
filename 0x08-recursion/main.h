3bf17a13e60b3ef1cb26f9174e09bfeffa97c0fb
yasuri02 <yasseralaamawy@gmail.com>
2023-07-05 11:35:41 -0700

    commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
@@ -10,3 +9,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+#endif
